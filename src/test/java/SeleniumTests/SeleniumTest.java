1e7cdcb05a449ed07288fe24e7ebf865820e227c
wrednazolza <wrednazolz@gmail.com>
2017-07-18 20:25:18 +0200

    Stworzenie klasy bazowej SeleniumTest

---
+++ src/test/java/SeleniumTests/SeleniumTest.java
@@ -0,0 +1,31 @@
+package SeleniumTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTest {
+
+    protected WebDriver driver;
+
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
